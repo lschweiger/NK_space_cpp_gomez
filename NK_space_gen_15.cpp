d9be8dedee4e3dde55a0a61ff0cde18a1a0e6443
Launy <lschweiger939@gmail.com>
2020-04-28 13:20:55 -0400

    function to replicate R code for scores generator

--- NK_space_gen_15.cpp
+++ NK_space_gen_15.cpp
@@ -13,11 +13,12 @@ using std::ofstream;
 #include <algorithm>//needed for min/max_element
 #include <ios>
 #include <iomanip>
+#include <cstdlib>
 
 //declare the uniform distribution, uni for use globally
 double num=240;
 double sigma=sqrt(1/num);
-std::normal_distribution<double> uni(0.7,.09);
+std::normal_distribution<double> uni(0.4,.09);
 std::random_device rd;
 std::mt19937 norm(rd());
 //call by reference for vector v to fill with new values of nk space with the new values from uniform (0,1)
@@ -30,52 +31,213 @@ void NKspacevals_gen(vector<double>& input_vec,int n){
 			//cout<<uni(rd)<<endl;
 		}
 };
-void NKspacevals_k(vector<double>& input_vec,int k_val){
-	int max_values=0;
-	if(k_val%2==0) max_values=1<<((k_val+1));
-	else max_values=1<<(k_val+1);
-	//cout<<max_values<<endl;
-	std::normal_distribution<double> hole1(.55,.009);
-	std::random_device rdn1;
-    std::mt19937 nor1(rdn1());
-    std::normal_distribution<double> hole2(.85,.009);
-	std::random_device rdn2;
-    std::mt19937 nor2(rdn2());
-    std::uniform_int_distribution<int> choice(0,(1<<15)-1);
-    std::random_device rdu;
-    std::mt19937 uniform(rdu());
-    int selection=0;
-    double replace=0.0;
-    for (int i = 0; i < max_values; ++i)
-    {
-    	selection=choice(uniform);
-    	while(input_vec[selection]==1) selection=choice(uniform);
-    	replace=hole1(nor1);
-    	while(replace>=1) replace=hole1(nor1);
-    	input_vec[selection]=replace;
-    }
-    for (int i = 0; i < max_values; ++i)
-    {
-    	selection=choice(uniform);
-    	while(input_vec[selection]==1) selection=choice(uniform);
-    	replace=hole2(nor2);
-    	while(replace>=1) replace=hole2(nor2);
-    	input_vec[selection]=replace;
-    }
+void NKspacevals_lowk(vector<double>& input_vec,int k_val){
+	int positions=(20-k_val)/2;
+	int max_pos=positions*2;
+	int max_num=(1<<(k_val+1));
+	for (uint i = 0; i < max_num; ++i)
+		{
+			std::uniform_int_distribution<int> selection(max_pos,input_vec.size()-max_pos);
+			std::random_device selrd;
+			std::mt19937 mtsel(selrd());
+			std::uniform_real_distribution<double> noise(.05,.015);
+			std::random_device nrd;
+			std::mt19937 mtnoise(nrd());
+			int hole_sel=selection(mtsel);
+			while(input_vec[hole_sel]==static_cast<double>(1.000))
+				{
+					hole_sel=selection(mtsel);
+				}
+			//cout<<hole_score<<" "<<hole_sel<<"\n";
+			input_vec[hole_sel]+=noise(mtnoise);
+			selection.reset();
+			noise.reset();
+		}
+	};
+void NKspacevals_highk(vector<double>& input_vec,int k_val){
+	int positions=(50-k_val)/2;
+	int max_pos=positions*2;
+	int max_num=(1<<(k_val+1));
+	for (uint i = 0; i < max_num; ++i)
+		{
+			std::uniform_real_distribution<double> replace(.65,.85);
+			std::random_device rprd;
+			std::mt19937 mt(rprd());
+			std::uniform_int_distribution<int> selection(max_pos,input_vec.size()-max_pos);
+			std::random_device selrd;
+			std::mt19937 mtsel(selrd());
+			std::uniform_real_distribution<double> noise(.001,.01);
+			std::random_device nrd;
+			std::mt19937 mtnoise(nrd());
+			double hole_score=replace(mt);
+			int hole_sel=selection(mtsel);
+			while(input_vec[hole_sel]==static_cast<double>(1.000))
+				{
+					hole_sel=selection(mtsel);
+				}
+			//cout<<hole_score<<" "<<hole_sel<<"\n";
+			input_vec[hole_sel]=hole_score;
+			int j=0;
+			double exp_pos=0.0;
+			for (j=1, exp_pos =5; j<positions; exp_pos-=(.2500),++j)
+				{
+					input_vec[hole_sel+j]=(hole_score*(1-std::exp(-exp_pos))-noise(mtnoise));
+					input_vec[hole_sel-j]=(hole_score*(1-std::exp(-exp_pos))-noise(mtnoise));
+				}
+		replace.reset();
+		selection.reset();
+		noise.reset();
+		}
+	};
+
+	double stdev_calc(vector<int> vec){//computes variance and standard deviation and returns standard deviation
+
+	double sum = std::accumulate(vec.begin(), vec.end(), 0.0); //
+	double mean = sum / vec.size();							   // computes sum and mean
+
+	std::vector<double> diff(vec.size());
+	std::transform(vec.begin(), vec.end(), diff.begin(), [mean](double x) { return x - mean; });// using transform to compute sum of X_i -X_mean; lambda function
+	double sq_sum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0); //using inner product to compute dot product for sum of squared
+	double stdev = std::sqrt(sq_sum / vec.size()); // standard deviation
+	return stdev;
+	};
+
+void NKspacevals_sine_normal(vector<double>& input_vec,int k_val){//call by reference; replicating R code, using Sine,Uniform, and Normal to generate scores
+	vector<int> sol(input_vec.size());
+	for (uint i = 0; i < (input_vec.size()); ++i)
+	{
+		sol[i]=i+1;	
+	}
+	double per1=(input_vec.size())/((1<<k_val));
+	double per2=(input_vec.size())/(1<<(k_val+3));
+	std::uniform_real_distribution<double> uniform1(0,1);
+	std::random_device rnd1;
+	std::mt19937 mt1(rnd1());
+	std::uniform_real_distribution<double> uniform2(0,1);
+	std::random_device rnd2;
+	std::mt19937 mt2(rnd2());
+	vector<double> score(input_vec.size());
+	vector<double> std_norm(input_vec.size());
+	std::uniform_int_distribution<int> norm_mean(1,(input_vec.size()));
+	std::random_device rndn_mean;
+	std::mt19937 mt_normmean(rndn_mean());
+	std::uniform_real_distribution<double> perturbation(.00005,.3);
+	std::random_device rndn_per;
+	std::mt19937 mt_per(rndn_per());
+	std::random_device rndn_norm;
+	std::mt19937 mt_norm(rndn_norm());
+	double uni1=uniform1(mt1);
+	double uni2=uniform2(mt2);
+	//#pragma omp simd
+	for (uint i = 0; i < input_vec.size(); ++i)
+	{
+		score[i]=(std::sin((1/per1)*(i+1))*uni1+std::sin((1/per2)*(i+1))*uni2);
+		//cout<<std::sin((1/per1)*(i+1))<<" "<<std::sin((1/per2)*(i+1))<<endl;
+		
+	}
+	//cout<<score[0]<<" value\n";
+	vector<double>::iterator maxresult;
+	vector<double>::iterator minresult;
+	maxresult = max_element(score.begin(), score.end());
+	minresult = min_element(score.begin(), score.end());
+	double maxscore=score[int(distance(score.begin(), maxresult))];
+	double minscore=score[int(distance(score.begin(), minresult))];
+	minscore=std::fabs(minscore);
+	double inverse_k= (1/k_val);
+	//#pragma omp simd
+	for (uint i = 0; i < input_vec.size(); ++i)
+	{
+		score[i]=((score[i]+minscore)/maxscore)*inverse_k;
+	}
+	/*for (uint i = 0; i < input_vec.size(); ++i)
+	{
+		score[i]=(inverse_k*(score[i]/maxscore));
+	}*/
+	double stdev = stdev_calc(sol);
+	stdev/=(1<<k_val);
+	double N_mean=static_cast<double>(norm_mean(mt_normmean));
+	double pi=atan(1) * 4;
+	double frac=(stdev*sqrtl(2.0*pi));
+	//#pragma omp simd
+	for (uint i = 0; i < input_vec.size(); ++i)
+	{
+		std_norm[i]=expl(pow((sol[i]-N_mean)/stdev,2)*(-0.5))/frac;
+		//cout<<expl(pow((sol[i]-N_mean)/stdev,2)*(-0.5));
+		
+		//cout<<"norm "<<std_norm[i]<<endl;
+	}
+	vector<double>::iterator maxresult_norm;
+	maxresult_norm = max_element(std_norm.begin(), std_norm.end());
+	double maxscore_norm=std_norm[int(distance(std_norm.begin(), maxresult_norm))];
+	//#pragma omp simd
+	for (uint i = 0; i < input_vec.size(); ++i)
+	{
+		//cout<<sol[i]<<endl;
+		std_norm[i]=std_norm[i]/maxscore_norm;
+		//cout<<"normd "<<std_norm[i]<<endl;
+
+	}
+	
+	std::vector<double> normd_scores(input_vec.size());
+	//#pragma omp simd
+	for (uint i = 0; i < input_vec.size(); ++i)
+	{
+		normd_scores[i]=std_norm[i]+score[i];
+	}
+	vector<double>::iterator maxresult_normd_score;
+	maxresult_normd_score = max_element(normd_scores.begin(), normd_scores.end());
+	double maxscore_normd_score=normd_scores[int(distance(normd_scores.begin(), maxresult_normd_score))];
+	//#pragma omp simd
+	for (uint i = 0; i < input_vec.size(); ++i)
+	{
+		normd_scores[i]=(normd_scores[i]/maxscore_normd_score)+perturbation(mt_per);
+		perturbation.reset();
+	}
+
+	/*for (uint i = 0; i < input_vec.size(); ++i)
+	{
+		normd_scores[i]=normd_scores[i]+perturbation(mt_per);
+		perturbation.reset();
+	}*/
+
+	/*
+	vector<double>::iterator pert_min;
+	pert_min = min_element(normd_scores.begin(), normd_scores.end());
+	double pert_min_score=normd_scores[int(distance(normd_scores.begin(), pert_min))];	
+	for (uint i = 0; i < input_vec.size(); ++i)
+	{
+		normd_scores[i]=normd_scores[i]/pert_min_score;
+	}
+	*/
+	vector<double>::iterator pert_normd;
+	pert_normd = max_element(normd_scores.begin(), normd_scores.end());
+	double pert_normd_score=normd_scores[int(distance(normd_scores.begin(), pert_normd))];
+	//#pragma omp simd
+	for (uint i = 0; i < input_vec.size(); ++i)
+	{
+		normd_scores[i]=normd_scores[i]/pert_normd_score;
+	}
+	input_vec=normd_scores;
+
 };
+
+
 //call by reference for vector v to fill with new values of nk space with the new values from uniform (0,1)
 void NKspacevals_unit(vector<double>& input_vec,int n){
 	vector<double>::iterator maxresult;
 	vector<double>::iterator minresult;
 	maxresult = max_element(input_vec.begin(), input_vec.end());
 	minresult = min_element(input_vec.begin(), input_vec.end());
+	input_vec[int(distance(input_vec.begin(), maxresult))]=double(1.000);
 	input_vec[int(distance(input_vec.begin(), minresult))]=double(0.000);
 	//cout << "max element at: " << int(distance(input_vec.begin(), maxresult))<<endl;
 	//cout << "min element at: " << int(distance(input_vec.begin(), minresult))<<endl;
-	for (int i = 0; i < n; ++i)
-		{	
-			input_vec[i]=input_vec[i]/input_vec[int(distance(input_vec.begin(), maxresult))];
-		}
+	/*
+		for (int i = 0; i < n; ++i)
+			{	
+				input_vec[i]=input_vec[i]/input_vec[int(distance(input_vec.begin(), maxresult))];
+			}
+	*/
 };
 
 int main(int argc, char *argv[]){
@@ -104,15 +266,18 @@ if (!file.is_open())
 
 int loop=1000;
 //creating and saving scores j<# is the number of spaces to create
-std::vector<int> k_opts={1,5,10};
+std::vector<int> k_opts={1,3,5,10};
 int start = 0;
 #pragma omp parallel for default(none) shared(k_opts,cout,nk,start,loop) firstprivate(v)
 	for (int j=start; j < loop; ++j)
 	{
 		for (int k = 0; k < k_opts.size(); ++k)
 		{
-			NKspacevals_gen(v,nk);
-			//NKspacevals_k(v,k_opts[k]);
+			NKspacevals_sine_normal(v,k_opts[k]);
+			//NKspacevals_gen(v,nk);
+			//if(k_opts[k]<10) NKspacevals_lowk(v,k_opts[k]);
+			//if(k_opts[k]==10) NKspacevals_highk(v,k_opts[k]);
+			
 			//NKspacevals_unit(v,nk);
 	//double sum = std::accumulate(v.begin(), v.end(), 0.0);
 	//double mean=sum / v.size();
